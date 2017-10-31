7b6cb487f3a5f8f8197d1a3bf87743d0c93908bd
PanDoes <pandasbuisness@gmail.com>
2017-10-30 23:42:30 -0700

    made calendar.js beautiful + fixed some bugs
    I actually commented stuff, and made my code good.

--- calendar.js
+++ calendar.js
@@ -1,185 +1,120 @@
-var curMonth = 0;
-var curDay = 0;
 var d = new Date();
-var thisDay = d.getDate();
-var thisMonth = d.getMonth();
-console.log("initialize" + curMonth);
-var monthTitle = 0; // 0 = Number titles, 1 = Month titles "Refer to calendar.js"
-var leapYear = leapYear();
+var gregMonth = d.getMonth(); // Current month of the year (Gregorian)
+var gregDay = d.getDate(); // Current day of the year (Gregorian)
+var curMonth = 0; // The month that is currently selected
+var curDay = 0; // The day that is currently selected (unused right now)
+var fixMonth = 0; // Current month of the year (Fixed I.)
+var fixDay = 0; // Current day of the year (Fixed I.)
+var monthTitle = 0; // 0 = Number titles, 1 = Month titles "Refer to updateMonth function"
+var leapYear = leapYear(); //Checks for a leap year.
+var fuckMonths = [31,28,31,30,31,30,31,31,30,31,30,31] // This is why I hate the Gregorian calendar.
+var dayOfYear = 0; // Day of the year as a number out of 364 (0 - 364)
+var x = 0; // Temporary variable
+
+// Possible Names for the months: (Maybe switch to csv?)
+var FixedTitle = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", 1];
+var GregTitle = ["January", "February", "March", "April", "May", "June", "Sol", "July", "August", "September", "October", "November", "December", 0];
+
+
+//Get the day of the year as a variable
+function findDayOfYear(){
+    dayOfYear = 0;
+
+    for (var i = 0; i < gregMonth; ++i){ // Makes dayOfYear equal to the current day of the year (- the days in the current month)
+        dayOfYear += fuckMonths[i];
+    }
+    dayOfYear += gregDay;
+    return dayOfYear;
+};
+// Cycle forward through the months
 function nxtMonth(){
-    console.log("nxtMonth Init" + curMonth);
     if (curMonth==12) {
         curMonth = 0;
     } else {
         curMonth = curMonth+1;
     }
     updateMonth(curMonth);
-    console.log("nxtMonth end" + curMonth);
-}
+};
+
+// Cycle backwards through the months
 function lstMonth(){
-    console.log("lstMonth Init" + curMonth);
     if (curMonth==0) {
         curMonth = 12;
     } else {
         curMonth = curMonth-1;
     }
     updateMonth(curMonth);
-    console.log("lstMonth end" + curMonth);
-}
+};
+
+// Change what title is being displayed
 function swapTitles(){
-    console.log("swap Init" + curMonth);
     switch(monthTitle){
         case 0:
             monthTitle = 1;
-        break;
-    case 1:
-        monthTitle = 0;
+            break;
+        case 1:
+            monthTitle = 0;
     }
-    updateMonth(curMonth);
-    console.log("swap end" + curMonth);
-}
-function findToday(){
-    var fuckMonths = [31,28,31,30,31,30,31,31,30,31,30,31]
-    var dayOfYear = 0;
-    var x = 0;
-    console.log("findToday Init" + thisMonth)
-    for (var i = 0; i < thisMonth; ++i){
-        dayOfYear += fuckMonths[i];
-    }
-    x = dayOfYear%28;
-    if(thisMonth > 5){
-        curMonth = ((dayOfYear-x)/28)+1;
-        thisMonth += 1;
-    }else{
-        curMonth = (dayOfYear-x)/28;
-    }
-    dayOfYear += thisDay;
-    thisDay = dayOfYear%28;
-    console.log(thisDay);
     updateMonth(curMonth);
 };
+
+// Convert the Gregorian day to Fixed I. day
+function findToday(){
+    dayOfYear -= gregDay; // Subtract days of the month
+    x = dayOfYear%28; // number of days off from Gregorian Calendar
+    fixMonth = ((dayOfYear-x)/28)
+    if(fixMonth >= 6){fixMonth+=1;}// Check if the month 'sol' has passed
+    dayOfYear += gregDay; // Re-add days of the month
+    fixDay = dayOfYear%28; // Get days into the month
+    updateMonth(fixMonth); // Update current month
+};
+
+// Check for leap year
 function leapYear(){
-    var d = new Date();
-    var result;
-    d = d.getFullYear();
+    x = 0;
+    fullYear = d.getFullYear();
     
-    if ((d%4 == 0) && (d%100 != 0) || (d%400 == 0)){
-        result = true;
+    // If year is divisible by 4, and not divisible by 100. Or if it is divisible by 400 then Leap Year.
+    if ((fullYear%4 == 0) && (fullYear%100 != 0) || (fullYear%400 == 0)){
+        x = true;
         console.log("leap year");
     } else{
-        result = false;
+        x = false;
         console.log("Not leap year");
     }
-    return result;
+    return x;
 };
-function updateMonth(curMonthNumber) {
-    var month;
+
+//Change the display of the current month (Please rewrite this soon)
+function updateMonth(monthNumber) {
+    x = 0;
     document.getElementById('yearDay').style.opacity = 0;
-    if (monthTitle == 0){
-        switch(curMonthNumber){
-            case 0:
-                month = "Zero";
-                break;
-            case 1:
-                month = "One";
-                break;
-            case 2:
-                month = "Two";
-                break;
-            case 3:
-                month = "Three";
-                break;
-            case 4:
-                month = "Four";
-                break;
-            case 5:
-                month = "Five";
-                if(leapYear){
-                    document.getElementById('yearDay').style.opacity = 1;
-                    document.getElementById('yearDayName').innerHTML = "Leap Day";
-                }                
-                break;
-            case 6:
-                month = "Six";
-                break;
-            case 7:
-                month = "Seven";
-                break;
-            case 8:
-                month = "Eight";
-                break;
-            case 9:
-                month = "Nine";
-                break;
-            case 10:
-                month = "Ten";
-                break;
-            case 11:
-                month = "Eleven";
-                break;
-            case 12:
-                month = "Twelve";
-                document.getElementById('yearDay').style.opacity = 1;
-                document.getElementById("yearDayName").innerHTML = "Year Day";
-                break;
-        }
-        document.getElementById("monthNum").innerHTML = month + " Month";
-    } else {
-        switch(curMonthNumber){
-            case 0:
-                month = "January";
-                break;
-            case 1:
-                month = "February";
-                break;
-            case 2:
-                month = "March";
-                break;
-            case 3:
-                month = "April";
-                break;
-            case 4:
-                month = "May";
-                break;
-            case 5:
-                month = "June";
-                if(leapYear){
-                    document.getElementById('yearDay').style.opacity = 1;
-                    document.getElementById('yearDayName').innerHTML = "Leap Day";
-                }    
-                break;
-            case 6:
-                month = "Sol";
-                break;
-            case 7:
-                month = "July";
-                break;
-            case 8:
-                month = "August";
-                break;
-            case 9:
-                month = "September";
-                break;
-            case 10:
-                month = "October";
-                break;
-            case 11:
-                month = "November";
-                break;
-            case 12:
-                month = "December";
-                document.getElementById('yearDay').style.opacity = 1;
-                document.getElementById('yearDayName').innerHTML = "Year Day";
-                break;
-        }
-        document.getElementById("monthNum").innerHTML = month;
+
+    // Check style, and asign "x" the text
+    switch(monthTitle){
+        case 0:
+            x = FixedTitle[monthNumber];
+            if(FixedTitle[13]){ x = "Month " + x;}
+            break;
+        case 1:
+            x = GregTitle[monthNumber]; 
+            if(GregTitle[13]){ x = "Month " + x;}
+            break;
     }
+    // Check Leap year and Year Day
+    if(monthNumber == 5 && leapYear){
+        document.getElementById('yearDay').style.opacity = 1;
+        document.getElementById('yearDayName').innerHTML = "Leap Day";
+    }  else if(monthNumber == 12){
+        document.getElementById('yearDay').style.opacity = 1;
+        document.getElementById("yearDayName").innerHTML = "Year Day";
+    }
+    document.getElementById("monthNum").innerHTML = x;
+    curMonth = monthNumber // Update curMonth to make it accurate.
     //Make sure current day is blue.
-    console.log(curMonth + " " + thisMonth);
-    if(curMonth==thisMonth){
-        console.log("We're in." + thisDay);      
-        document.getElementById(thisDay).style.backgroundColor = '#00bfff';
+    if(curMonth==fixMonth){    
+        document.getElementById(fixDay).style.backgroundColor = '#00bfff';
     }else{
-        document.getElementById(thisDay).style.backgroundColor = '#333';
+        document.getElementById(fixDay).style.backgroundColor = '#333';
     }
 };
